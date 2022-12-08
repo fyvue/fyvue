028e6dd9ad98b37c088313d848191c7b9dcc0ebc
Florian Gasquez <m@fy.to>
2022-12-08 09:51:31 +0100

    Update LayoutHeader.vue

--- src/components/LayoutHeader.vue
+++ src/components/LayoutHeader.vue
@@ -1,4 +1,6 @@
-<script setup>
+<script setup lang="ts">
+import { ref } from "vue";
+import { MagnifyingGlassIcon } from "@heroicons/vue/24/solid";
 const navLinks = [
   { name: "Home", to: "/" },
   {
@@ -11,11 +13,41 @@ const navLinks = [
   },
   { name: "Contact", to: "/contact" },
 ];
+const query = ref<string>();
 </script>
 <template>
   <NavMain :links="navLinks" :showTitle="true" title="website">
     <template v-slot:logo>
       <img src="@/assets/images/logo.svg" class="h-10" />
+    </template>
+    <template v-slot:custom>
+      <div class="fyui-nav__search">
+        <form
+          class=""
+          @submit.prevent="
+            () => {
+              if (query) $router.push(`/search/${query}`);
+              else {
+                $router.push(`/search/`);
+              }
+            }
+          "
+        >
+          <DefaultInput
+            id="search"
+            ref="searchInputRef"
+            v-model="query"
+            type="search"
+            placeholder="Search..."
+          >
+            <template v-slot:before>
+              <div class="fyuid-nav__search-icon">
+                <MagnifyingGlassIcon class="w-5 h-5 text-fv-neutral-500" />
+              </div>
+            </template>
+          </DefaultInput>
+        </form>
+      </div>
     </template>
   </NavMain>
 </template>
